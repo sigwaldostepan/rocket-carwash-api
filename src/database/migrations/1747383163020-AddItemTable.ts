501a1cfbbe4a34a3549d89742d1ea72df1ee7783
sigwaldostepan <sigwaldos@gmail.com>
2025-05-16 15:21:51 +0700

    feat(item): create table migration

---
+++ src/database/migrations/1747383163020-AddItemTable.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class AddItemTable1747383163020 implements MigrationInterface {
+    name = 'AddItemTable1747383163020'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "item" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "price" numeric NOT NULL, CONSTRAINT "PK_d3c0c71f23e7adcf952a1d13423" PRIMARY KEY ("id"))`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "item"`);
+    }
+
+}
